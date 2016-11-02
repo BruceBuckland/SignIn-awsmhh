480e1f3af60aad03a3fad32ee16ac4c118e56be4
Bruce Buckland <bruce@odellventures.com>
2016-11-02 12:08:04 -0400

    Locked aws-mobilehub-helper-ios at v0.0b1.0

---
+++ SignIn-awsmhh-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SignIn-awsmhh-Bridging-Header.h
+//  SignIn-awsmhh
+//
+//  Created by Bruce Buckland on 10/22/16.
+//  Copyright © 2016 Bruce Buckland. All rights reserved.
+//
+
+#ifndef SignIn_awsmhh_Bridging_Header_h
+#define SignIn_awsmhh_Bridging_Header_h
+
+#import "AWSMobileHubHelper.h"
+
+#endif /* SignIn_awsmhh_Bridging_Header_h */
+
+
